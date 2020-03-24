3c997c30c434ea2bf1a6e7d3bd4a7f6fd60fbf8f
Cecile Muller <cecilemuller@users.noreply.github.com>
2020-03-24 23:08:18 +0100

    chore: re-format sources using Prettier 2

--- src/getConfig.js
+++ src/getConfig.js
@@ -1,13 +1,12 @@
 /* eslint-env node */
 /* eslint-disable no-process-env */
-'use strict';
-const {strictEqual} = require('assert');
-const puppeteer = require('puppeteer');
+"use strict";
+const {strictEqual} = require("assert");
+const puppeteer = require("puppeteer");
 
-if (process && process.env){
+if (process && process.env) {
 	process.env.CHROME_BIN = puppeteer.executablePath();
 }
-
 
 /**
  * @typedef Config
@@ -19,21 +18,21 @@ if (process && process.env){
  * Generates a config for Karma.
  * @param {Config} options
  */
-function getConfig({files = 'src/**/*.spec.js', webpack} = {}){
+function getConfig({files = "src/**/*.spec.js", webpack} = {}) {
 	//region Files
-	const patterns = (typeof files === 'string') ? [files] : files;
-	if (!Array.isArray(patterns)){
+	const patterns = typeof files === "string" ? [files] : files;
+	if (!Array.isArray(patterns)) {
 		throw new Error('"files" should be a String, or an Array of String');
 	}
 	const l = patterns.length;
-	if (l === 0){
+	if (l === 0) {
 		throw new Error('"files" should not be an empty Array');
 	}
-	for (const pattern of patterns){
-		if (typeof pattern !== 'string'){
+	for (const pattern of patterns) {
+		if (typeof pattern !== "string") {
 			throw new Error('"files" should be a String, or an Array of String');
 		}
-		if (pattern === ''){
+		if (pattern === "") {
 			throw new Error('"files" should not have an empty String');
 		}
 	}
@@ -41,12 +40,12 @@ function getConfig({files = 'src/**/*.spec.js', webpack} = {}){
 	//region Base config
 	const config = {
 		mime: {},
-		browsers: ['ChromeHeadless'],
-		frameworks: ['jasmine'],
+		browsers: ["ChromeHeadless"],
+		frameworks: ["jasmine"],
 		client: {
 			jasmine: {
 				random: true,
-				seed: '4321',
+				seed: "4321",
 				stopOnFailure: true,
 				failFast: true
 			}
@@ -60,25 +59,29 @@ function getConfig({files = 'src/**/*.spec.js', webpack} = {}){
 	strictEqual(webpack instanceof Promise, false, '"webpack" should not be a Promise');
 	strictEqual(webpack instanceof RegExp, false, '"webpack" should not be a RegExp');
 	strictEqual(webpack instanceof Symbol, false, '"webpack" should not be a Symbol');
-	strictEqual((typeof webpack === 'object') || (typeof webpack === 'undefined'), true, '"webpack" should be an Object or be undefined');
-	if (typeof webpack === 'object'){
+	strictEqual(
+		typeof webpack === "object" || typeof webpack === "undefined",
+		true,
+		'"webpack" should be an Object or be undefined'
+	);
+	if (typeof webpack === "object") {
 		// https://github.com/karma-runner/karma-chrome-launcher/issues/176#issuecomment-381642145
-		config.mime['text/x-typescript'] = ['ts', 'tsx'];
+		config.mime["text/x-typescript"] = ["ts", "tsx"];
 
 		const webpackCopy = Object.assign({}, webpack);
 		delete webpackCopy.entry;
 		delete webpackCopy.optimization;
 		config.webpack = webpackCopy;
 		config.webpackMiddleware = {
-			logLevel: 'silent'
+			logLevel: "silent"
 		};
-		if (typeof webpackCopy.mode === 'undefined'){
-			webpackCopy.mode = 'development';
+		if (typeof webpackCopy.mode === "undefined") {
+			webpackCopy.mode = "development";
 		}
 
 		const preprocessors = {};
-		for (const pattern of patterns){
-			preprocessors[pattern] = ['webpack', 'sourcemap'];
+		for (const pattern of patterns) {
+			preprocessors[pattern] = ["webpack", "sourcemap"];
 		}
 		config.preprocessors = preprocessors;
 	}
